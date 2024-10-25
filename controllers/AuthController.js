d40d4aac201b7f20d30d92ef69fbd09b0b57bada
carenoizabel <miatellogmonteiro@gmail.com>
2024-10-25 00:43:08 -0400

    Update Aula 5
    Implementação do cadastro no auth, com o bcryptjs criptografamos as senhas dos usuarios oq a senha real fica totalmente diferente, validamos os dados recebidos do usuario com o email e senha e nome e que tem alguns erros que é importante sempre ter em toda aplicação, e fizemos o teste para isso, e criamos um js que ao executar lista os usuarios

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const bcryptjs = require("bcryptjs");
+
 class AuthController {
   static async login(req, res) {
   }
@@ -14,11 +16,58 @@ class AuthController {
         });
     }
 
-    return res.json({
-        erro: false,
-        mensagem: "Usuario cadastrado com sucesso!",
-        token: "success",
+    if(!email || email.length < 10){
+        return res.json({
+            erro: true,
+            mensagem: "O email deve ter pelo menos 10 caracteres.",
+        });
+    }
+
+    if(!password || password.length < 8){
+        return res.json({
+            erro: true,
+            mensagem: "A senha deve ter pelo menos 8 caracteres.",
+        });
+    }
+
+    const existe = await prisma.usuario.count({
+      where: {
+        email: email,
+      },
     });
+    
+    if (existe != 0){
+      return res.json({
+        erro: true,
+        mensagem: "Já existe um usuário cadastrado com este e-mail.",
+      })
+    }
+
+    const salt = bcryptjs.genSaltSync(8);
+    const hashPassword = bcryptjs.hashSync(password, salt);
+
+try {
+  const usuario = await prisma.usuario.create({
+    data: {
+      nome: nome,
+      email: email,
+      password: hashPassword,
+      tipo: "cliente",
+    }
+  });
+  return res.json({
+    erro: false,
+    mensagem: "Usuario cadastrado com sucesso!",
+});
+} catch (error) {
+  return res.json({
+    erro: true,
+    mensagem: "Ocorreu um erro, tente novamente mais tarde!" + error,
+});
+}
+  }
+  static async login(req, res) {
+    
   }
 }
 
