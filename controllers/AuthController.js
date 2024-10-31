c45bb094bcae93e2ba84bee48ea843ac29a77b6f
carenoizabel <miatellogmonteiro@gmail.com>
2024-10-31 16:19:18 -0400

    Update Aula 6
    Nessa aula é para configurar a rota login que requer apenas email e senha,  arrumamos o shows para quando mostrar os usuarios mostre ja formatado.
    Ai implementamos a funncionalidade de login no controlador, incluindo o capturar, buscar, ver se o usuario existe  no caso os erros, como exemplo se o usuario não é encontrado ele exibe uma mensagem.
    
    Depois usamos o compare do bcrypt se a senha estiver correta um token jwt e gerado, no token inclui id e e crptografado com uma secreta no .env

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,31 +3,31 @@ const prisma = new PrismaClient();
 
 const bcryptjs = require("bcryptjs");
 
+const jwt = require("jsonwebtoken");
+
 class AuthController {
-  static async login(req, res) {
-  }
   static async cadastro(req, res) {
     const { nome, email, password } = req.body;
 
-    if(!nome || nome.length <6){
-        return res.json({
-            erro: true,
-            mensagem: "O nome deve ter pelo menos 6 caracteres.",
-        });
+    if (!nome || nome.length < 6) {
+      return res.json({
+        erro: true,
+        mensagem: "O nome deve ter pelo menos 6 caracteres.",
+      });
     }
 
-    if(!email || email.length < 10){
-        return res.json({
-            erro: true,
-            mensagem: "O email deve ter pelo menos 10 caracteres.",
-        });
+    if (!email || email.length < 10) {
+      return res.json({
+        erro: true,
+        mensagem: "O email deve ter pelo menos 10 caracteres.",
+      });
     }
 
-    if(!password || password.length < 8){
-        return res.json({
-            erro: true,
-            mensagem: "A senha deve ter pelo menos 8 caracteres.",
-        });
+    if (!password || password.length < 8) {
+      return res.json({
+        erro: true,
+        mensagem: "A senha deve ter pelo menos 8 caracteres.",
+      });
     }
 
     const existe = await prisma.usuario.count({
@@ -35,39 +35,78 @@ class AuthController {
         email: email,
       },
     });
-    
-    if (existe != 0){
+
+    if (existe != 0) {
       return res.json({
         erro: true,
         mensagem: "Já existe um usuário cadastrado com este e-mail.",
-      })
+      });
     }
 
     const salt = bcryptjs.genSaltSync(8);
     const hashPassword = bcryptjs.hashSync(password, salt);
 
-try {
-  const usuario = await prisma.usuario.create({
-    data: {
-      nome: nome,
-      email: email,
-      password: hashPassword,
-      tipo: "cliente",
+    try {
+      const usuario = await prisma.usuario.create({
+        data: {
+          nome: nome,
+          email: email,
+          password: hashPassword,
+          tipo: "cliente",
+        },
+      });
+
+      console.log(JSON.stringify(usuario));
+      const token = jwt.sign({ id: usuario.id }, process.env.SECRET_KEY, {
+        expiresIn: "1h",
+      });
+
+      return res.json({
+        erro: false,
+        mensagem: "Usuario cadastrado com sucesso!",
+      });
+    } catch (error) {
+      return res.json({
+        erro: true,
+        mensagem: "Ocorreu um erro, tente novamente mais tarde!" + error,
+      });
     }
-  });
-  return res.json({
-    erro: false,
-    mensagem: "Usuario cadastrado com sucesso!",
-});
-} catch (error) {
-  return res.json({
-    erro: true,
-    mensagem: "Ocorreu um erro, tente novamente mais tarde!" + error,
-});
-}
   }
   static async login(req, res) {
-    
+    const { email, password } = req.body;
+
+    const usuario = await prisma.usuario.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!usuario) {
+      return res.json({
+        erro: true,
+        mensagem: "Usuário não encontrado.",
+      });
+    }
+
+    //Verificação da senha
+
+    const senhaCorreta = bcryptjs.compareSync(password, usuario.password);
+
+    if (!senhaCorreta) {
+      return res.json({
+        erro: true,
+        mensagem: "Senha incorreta.",
+      });
+    }
+    const token = jwt.sign({ id: usuario.id }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      erro: false,
+      mensagem: "Autenticação realizada com sucesso!",
+      token: token,
+    })
   }
 }
 
