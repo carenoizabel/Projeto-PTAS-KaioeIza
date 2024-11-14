3bcd48de12d98247e706386d56781219da4070f3
carenoizabel <izabelcareno12345@gmail.com>
2024-11-14 09:12:10 -0400

    adicionando o cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const prisma = require("./prisma/prismaClient");
 
+const cors = require("cors");
+ 
 const express = require('express');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true}));
